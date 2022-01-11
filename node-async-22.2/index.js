096eee0b31105615f61e3f17c6c106f727847129
rphsantos1995 <rph.santos@Outlook.com>
2022-01-10 23:06:36 -0300

    ex3

--- node-async-22.2/index.js
+++ node-async-22.2/index.js
@@ -9,7 +9,7 @@ function readNumberPromise (num1, num2, num3) {
       'number' || typeof(num3) !== 
       'number') ) reject(new Error ("Informe apenas números"))
     const result = ((num1 + num2) * num3);
-    if (result < 5000) reject(new Error ("Valor muito baixo"))
+    if (result < 2000) reject(new Error ("Valor muito baixo"))
     resolve(result);
   });
 }
@@ -17,11 +17,30 @@ function readNumberPromise (num1, num2, num3) {
 
 // Exercício 2 - Escreva um código para consumir a função construída no exercício anterior.
 
-const getRandom = () => {
+const getRandomWithPromise = () => {
   let num1 = Math.floor(Math.random() * 100 + 1);
   let num2 = Math.floor(Math.random() * 100 + 1);
   let num3 = Math.floor(Math.random() * 100 + 1);
   readNumberPromise(num1, num2, num3).then(data => console.log(data)).catch(err => console.log(err));
 }
 
-getRandom();
+// Exercício 3 -  Reescreva o código do exercício anterior para que utilize async/await
+
+
+const getRandomWithAwait = async () => {
+  let num1 = Math.floor(Math.random() * 100 + 1);
+  let num2 = Math.floor(Math.random() * 100 + 1);
+  let num3 = Math.floor(Math.random() * 100 + 1);
+  try {
+    const result = await readNumberPromise(num1, num2, num3);
+    console.log(result);
+    return result;
+    
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getRandomWithAwait();
+
+
