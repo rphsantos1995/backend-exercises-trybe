1d679b755e7292952e7849cb89304ed2fe86ba47
rphsantos1995 <rph.santos@Outlook.com>
2022-01-18 18:10:08 -0300

    first express app

---
+++ node-express-http/hello-express/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express(); // 1
+
+app.get('/hello', handleHelloWorldRequest); // 2
+app.get('/', handleMainPageRequest);
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+}); // 3
+
+function handleHelloWorldRequest(req, res) {
+  res.status(200).send(`Apenas mais um exemplo de hello world aleatório, acredito que deveria 
+  ser um pouco mais costumizado.`); // 4
+}
+
+function handleMainPageRequest(req, res) {
+  res.status(200).send(`Página inicial para testar esse express`);
+}
